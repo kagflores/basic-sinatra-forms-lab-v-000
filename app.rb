632bb095af78346157c21694badbc9b34851b398
Karen Flores <karen.ag.flores@gmail.com>
2019-10-09 22:04:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,6 +9,11 @@ class App < Sinatra::Base
   post '/newteam' do
     @teamname = params["name"]
     @coach = params["coach"]
+    @point = params["pg"]
+    @shootg = params["sg"]
+    @powerf = params["pf"]
+    @smallf = params["sf"]
+    @center = params["c"]
     erb :team
   end
 
